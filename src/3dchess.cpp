dc1e23959d7862f8953286bef4ab5beb316e48fa
silent <hszwnts@gmx.com>
2013-11-12 20:10:43 +0100

    Moved window functions into GuiWindow.cpp

--- src/3dchess.cpp
+++ src/3dchess.cpp
@@ -1,5 +1,4 @@
 #include <iostream>
-#include <SDL.h>
 #include <cmath>
 #include <fstream>
 
@@ -13,14 +12,7 @@
 #include "DummyPlayer.h"
 #include "GameConfiguration.h"
 #include "ObserverDispatcherProxy.h"
-
-#ifdef _WIN32
-#include <windows.h> // Needed for gl.h
-#endif
-
-#include <GL/gl.h>
-
-#include <SOIL.h>
+#include "GuiWindow.h"
 
 namespace po = boost::program_options;
 namespace sig = boost::signals2;
@@ -50,19 +42,10 @@ int main(int argn, char **argv) {
 	po::options_description desc("Test");
 	desc.add_options()
 			("help", "Print help message")
-			("width", po::value<int>()->default_value(640), "Horizontal resolution")
-			("height", po::value<int>()->default_value(480), "Vertical resolution")
+			("width", po::value<int>()->default_value(1024), "Horizontal resolution")
+			("height", po::value<int>()->default_value(768), "Vertical resolution")
 			("fullscreen", "If set program runs in fullscreen")
 			;
-
-	GLuint tex_2d = SOIL_load_OGL_texture
-		(
-		"img.png",
-		SOIL_LOAD_AUTO,
-		SOIL_CREATE_NEW_ID,
-		SOIL_FLAG_MIPMAPS | SOIL_FLAG_INVERT_Y | SOIL_FLAG_NTSC_SAFE_RGB | SOIL_FLAG_COMPRESS_TO_DXT
-		);
-	(void)tex_2d;
 
 	po::variables_map vm;
 	po::store(po::parse_command_line(argn, argv, desc), vm);
@@ -105,75 +88,8 @@ int main(int argn, char **argv) {
 	cout << "Height:        " << height << endl;
 	cout << "Fullscreen:    " << fullscreen << endl;
 
-	SDL_Window *window;
+	GuiWindow *window = new GuiWindow("3D Chess", fullscreen, width, height);
+	window->exec();
 
-	if (SDL_Init(SDL_INIT_VIDEO) < 0) {
-		cerr << "Failed to init SDL: " << SDL_GetError() << endl;
-		return 1;
-	}
-
-	SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
-	SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 24);
-
-	const int additional_config_flags = fullscreen ? SDL_WINDOW_FULLSCREEN : 0;
-
-	window = SDL_CreateWindow("Hello chess",
-							  SDL_WINDOWPOS_UNDEFINED,
-							  SDL_WINDOWPOS_UNDEFINED,
-							  width, height,
-							  SDL_WINDOW_OPENGL | additional_config_flags);
-
-	if (window == nullptr) {
-		cerr << "Failed to create window: " << SDL_GetError() << endl;
-		return 1;
-	}
-
-	SDL_GLContext ogl = SDL_GL_CreateContext(window);
-	
-	SDL_GL_SetSwapInterval(1);
-
-	bool quit = false;
-	SDL_Event evt;
-
-	const uint32_t start = SDL_GetTicks();
-	const float pi = static_cast<float>(M_PI);
-
-	while (!quit) {
-		const float elapsed = static_cast<float>(SDL_GetTicks() - start) / 1000;
-
-		const float cycletime = 5.0f;
-		const float r = sinf(elapsed * (2 * pi) / cycletime);
-		const float g = cosf(elapsed * (2 * pi) / (cycletime + 1));
-		const float b = sinf(elapsed * (2 * pi) / (cycletime + 2) + pi / 2);
-
-		glClearColor(r,g,b, 1.0);
-		glClear(GL_COLOR_BUFFER_BIT);
-		SDL_GL_SwapWindow(window);
-
-		while (SDL_PollEvent(&evt)) {
-			switch (evt.type) {
-			case SDL_QUIT:
-				cout << "Quit" << endl;
-				quit = true;
-				break;
-			case SDL_KEYDOWN:
-				if (evt.key.keysym.sym == SDLK_ESCAPE) {
-					quit = true;
-				}
-
-				cout << "Keydown: " << evt.key.keysym.sym << endl;
-				break;
-			default: break;
-			}
-		}
-		
-		// Execute all pending calls from the observer
-		observerProxy->poll();
-	}
-
-	SDL_GL_DeleteContext(ogl);
-	SDL_DestroyWindow(window);
-
-	SDL_Quit();
 	return 0;
 }
