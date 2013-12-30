260c522c99be0cf24cb930c262f3b44b82b76f0f
silentHoo <silent@gmx.biz>
2013-12-30 18:40:20 +0100

    Fixed some issues. - Correction vectors of models fixed. The models are now at the correct y level. - Prepared class for animating models when performing a turn. - First step towards better caching strategy. - Changed some minor things.

--- src/gui/ChessSet.cpp
+++ src/gui/ChessSet.cpp
@@ -7,12 +7,15 @@
 
 using namespace std;
 
-ChessSet::ChessSet() {
-	m_tileWidth = 24;
-	m_tileHeight = 2;
+ChessSet::ChessSet()
+	: m_tileWidth(24) 
+	, m_tileHeight(2)
+	, m_turnMoveShowDuration(2000)
+	, m_turnMoveShownSince(0)
+	, m_lastTurnAvailable(false) {
 
 	// all external resources to load
-	extResources = {
+	m_extResources = {
 		"resources/3dmodels/king.3DS",
 		"resources/3dmodels/queen.3DS",
 		"resources/3dmodels/bishop.3DS",
@@ -21,12 +24,12 @@ ChessSet::ChessSet() {
 		"resources/3dmodels/pawn.3DS"
 	};
 
-    extCorrectionValues = { {
-        { 61, 11, 0, 1, -90, 0, 0 },	// king
-        { 40, 11, 0, 1, -90, 0, 0 },	// queen
-        { 1, 19, 0, 1, -90, 0, 0 },		// bishop
+	m_extCorrectionValues = { {
+        { 61, 12, 0, 1, -90, 0, 0 },	// king
+        { 40, 12, 0, 1, -90, 0, 0 },	// queen
+        { 1, 21, 0, 1, -90, 0, 0 },		// bishop
         { 0, 0, 0, 0.4, -90, 0, 0 },	// knight
-        { 20, 11, 0, 1, -90, 0, 0 },	// rook
+        { 20, 12, 0, 1, -90, 0, 0 },	// rook
         { 0, 12, 0, 1, -90, 0, 0 }		// pawn
     } };
 }
@@ -36,22 +39,22 @@ ChessSet::~ChessSet() {
 }
 
 int ChessSet::getResourcesCount() {
-	return extResources.size() + 1;	// +1 for the board
+	return m_extResources.size() + 1;	// +1 for the board
 }
 
 void ChessSet::loadResources() {
 	// load models
 	int i = 0;
-	for (auto &resName : extResources) {
+	for (auto &resName : m_extResources) {
 		// propagate what is loaded
 		m_loadCallback(resName);
 
 		// create the model
-		models[i] = make_shared<Model>(resName);
+		m_models[i] = make_shared<Model>(resName);
 
 		// set the correction values for the model
-        auto& cor = extCorrectionValues[i];
-        models[i]->setCorrectionValues(
+		auto& cor = m_extCorrectionValues[i];
+		m_models[i]->setCorrectionValues(
             cor.x, cor.y, cor.z,
             cor.scale,
             cor.rotX, cor.rotY, cor.rotZ
@@ -60,19 +63,19 @@ void ChessSet::loadResources() {
 		// cache the model in a display list
 
 		// white models
-		models[i]->setColor(Model::Color::WHITE);
+		m_models[i]->setColor(Model::Color::WHITE);
 		m_modelList[i] = glGenLists(1);
 
 		glNewList(m_modelList[i], GL_COMPILE);
-			models[i]->draw();
+			m_models[i]->draw();
 		glEndList();
 
 		// black models
-		models[i]->setColor(Model::Color::BLACK);
+		m_models[i]->setColor(Model::Color::BLACK);
 		m_modelList[i+6] = glGenLists(1);
 
 		glNewList(m_modelList[i+6], GL_COMPILE);
-			models[i]->draw();
+			m_models[i]->draw();
 		glEndList();
 
 		++i;
@@ -88,40 +91,86 @@ void ChessSet::registerLoadCallback(const boost::function<void(std::string)>& sl
 	m_loadCallback.connect(slot);
 }
 
-void ChessSet::setState(std::array<Piece, 64> state) {
+void ChessSet::setState(std::array<Piece, 64> state, PlayerColor lastPlayer, Turn lastTurn) {
 	m_state = state;
+	m_lastPlayer = lastPlayer;
+	m_lastTurn = lastTurn;
+
+	m_lastTurnAvailable = true;
+
+	if (m_lastTurn.action == Turn::Action::Move) {
+		m_turnMoveShownSince = SDL_GetTicks();
+	}
 }
 
 void ChessSet::draw() {
-	/*if (m_turns.back().action == Turn::Action::Move) {
-		// todo
-	}*/
+	drawModels();
+	drawBoard();
+}
 
-	int field = 0;
-	for (auto &p : m_state) {
-		if (p.type != PieceType::NoType) {
-			int row = field % 8;
-			int col = field / 8;
-
-			glPushMatrix();
-				// move model to tile
-				glTranslatef(((row - 4.f) * m_tileWidth) + (m_tileWidth / 2.f), 0, ((col - 4.f) * m_tileWidth) + (m_tileWidth / 2.f));
-
-				// draw model via list index
-				int listIndex = p.type + (p.player == PlayerColor::Black ? 6 : 0);
-				glCallList(m_modelList[listIndex]);
-			glPopMatrix();
-		}
-		++field;
+void ChessSet::drawModels() {
+	// if a new turn is made, create a new model list without the turn dependent models
+	if (m_lastTurnAvailable) {
+		createModelsList(true);
+		m_lastTurnAvailable = false;
 	}
 
-	// chessboard
+	// do not animate anything if time is up
+	if (m_turnMoveShownSince < (SDL_GetTicks() - m_turnMoveShowDuration)) {
+		animateModelTurn();
+	} else {
+		// if time is up -> create a model list with all models
+		createModelsList(false);
+	}
+	
+	glCallList(m_modelsList);
+}
+
+void ChessSet::drawBoard() {
 	glCallList(m_boardList);
 }
 
 // replace by translation
 void ChessSet::moveModelToTile(ModelPtr model, int row, int col) {
 	glTranslatef(((row - 4.f) * m_tileWidth) + (m_tileWidth / 2.f), static_cast<float>(m_tileHeight), ((col - 4.f) * m_tileWidth) + (m_tileWidth / 2.f));
+}
+
+// create the list only the first time
+// to update single model positions
+void ChessSet::createModelsList(bool withoutTurnDependentModels) {
+	// draw all models without the model on position .from and .to
+	// these models will be drawn in a separate method with animation
+	// -> fade out .to model
+	// -> move animation .from model
+	m_modelsList = glGenLists(1);
+	glNewList(m_modelsList, GL_COMPILE);
+		int field = 0;
+		for (auto &p : m_state) {
+			if (p.type != PieceType::NoType) {
+				int row = field % 8;
+				int col = field / 8;
+				
+				glPushMatrix();
+					
+//					if (withoutTurnDependentModels || (field != m_lastTurn.from && field != m_lastTurn.to)) {
+						// move model to tile
+						glTranslatef(((row - 4.f) * m_tileWidth) + (m_tileWidth / 2.f), 0, ((col - 4.f) * m_tileWidth) + (m_tileWidth / 2.f));
+
+						// draw model via list index
+						int listIndex = p.type + (p.player == PlayerColor::Black ? 6 : 0);
+						glCallList(m_modelList[listIndex]);
+//					}
+					
+				glPopMatrix();
+			}
+			++field;
+		}
+	glEndList();
+}
+
+void ChessSet::animateModelTurn() {
+	
+
 }
 
 void ChessSet::createChessBoardList() {
--- src/gui/ChessSet.h
+++ src/gui/ChessSet.h
@@ -21,40 +21,50 @@ public:
 	ChessSet();
 	~ChessSet();
 
-	void draw();
-	void setState(std::array<Piece, 64> state);
+	/**
+	 * @brief Sets the new chess state.
+	 * @note You need to call this only, when there's a visible change like moving a
+	 *       figure from field A to field B.
+	 * @param state The state of the current board. Only the given pieces on the fields
+	 *              will be drawn.
+	 */
+	void setState(std::array<Piece, 64> state, PlayerColor lastPlayer, Turn lastTurn);
+
+	/**
+	 * @brief Returns the number of big resources which must be loaded for initializing
+	 *        the ChessSet.
+	 * @note This can be used for a progress bar.
+	 * @return The number of big resources.
+	 */
 	int getResourcesCount();
 
-	// loads all the resources
-	void loadResources();
+	/**
+	 * @brief Registers a function as callback.
+	 * @param callback The function which will be called when a resource was
+	 *        successfully loaded.
+	 */
 	void registerLoadCallback(const boost::function<void(std::string)>& callback);
 
-	enum ModelType {
-		KING,
-		PAWN,
-		QUEEN,
-		BISHOP,
-		KNIGHT,
-		ROOK
-	};
+	/**
+	 * @brief Loads all resources, builds the models and the chess board.
+	 * @note If you've registered a function as callback, you will be informed
+	 *       on each resource which is loaded.
+	 */
+	void loadResources();
 
-	enum ModelColor {
-		BLACK,
-		WHITE
-	};
-
-	enum BoardTile {
-
-	};
-
-protected:
-	int m_tileWidth, m_tileHeight;
+	/**
+	* @brief Draws the whole ChessSet. This includes all models and the chess board.
+	*        Depending in the current state.
+	*/
+	void draw();
 
 private:
+	int m_tileWidth, m_tileHeight;
+
 	// each figure is one time present in memory, so we are stupid here and only draw what we get with the bitboard
 	ModelPtr king, pawn, queen, bishop, knight, rook;
-	std::array<ModelPtr, 6> models;
-	std::vector<std::string> extResources;
+	std::array<ModelPtr, 6> m_models;
+	std::vector<std::string> m_extResources;
 
     struct CorrectionValue {
         int x, y, z;
@@ -62,14 +72,25 @@ private:
         int rotX, rotY, rotZ;
     };
 
-	std::array<CorrectionValue, 6> extCorrectionValues;	// 6 models a 7 parameters
+	std::array<CorrectionValue, 6> m_extCorrectionValues;	// 6 models a 7 parameters
 
 	// OpenGL DisplayLists
-	GLuint m_boardList;
-	GLuint m_modelList[12];	// first 6 white, last 6 black
-	std::array<Piece, 64> m_state;
+	GLuint m_boardList;				// 8x8 fields
+	GLuint m_modelList[12];			// first 6 white, last 6 black
+	GLuint m_modelsList;			// models translated and models itself
+	std::array<Piece, 64> m_state;	// whole chess state
+
+	unsigned int m_turnMoveShowDuration;
+	unsigned int m_turnMoveShownSince;
+	bool m_lastTurnAvailable;
+	Turn m_lastTurn;
+	PlayerColor m_lastPlayer;
 
 	void createChessBoardList();
+	void createModelsList(bool withoutTurnDependentModels);
+	void animateModelTurn();
+	void drawModels();
+	void drawBoard();
 	void drawTile(int x, int y, int z, bool odd, bool highlight);
 	void moveModelToTile(ModelPtr model, int row, int col);
 
