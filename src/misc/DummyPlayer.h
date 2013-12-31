1c004b6f011b4b34f0aaf301bf71d1fd8641b2ec
Stefan Hacker <dd0t@users.sourceforge.net>
2013-12-31 14:32:25 +0100

    Seed DummyPlayer RNG from a random device to get varied behavior

--- src/misc/DummyPlayer.h
+++ src/misc/DummyPlayer.h
@@ -19,20 +19,25 @@
 class DummyPlayer : public AbstractPlayer, public ServiceDispatcherThread {
 public:
     DummyPlayer()
-        : m_rng()
+        : m_seed(std::random_device()())
+        , m_rng(m_seed)
         , m_msDist(0, 5)
-        , m_log(Logging::initLogger("DummyPlayer")) {}
+        , m_log(Logging::initLogger("DummyPlayer")) {
+        LOG(Logging::info) << "Seed: " << m_seed;
+    }
     
     virtual ~DummyPlayer() {
         stop(true);
     }
     
     virtual void onSetColor(PlayerColor color) override {
-        LOG(Logging::info) << "Will be playing " << color;
+        LOG(Logging::info) << "(" << m_seed << ") "
+                           << "Will be playing " << color;
     }
     
     virtual void doAbortTurn() override {
-        LOG(Logging::warning) << "Asked to abort turn. This is not implemented";
+        LOG(Logging::warning) << "(" << m_seed << ") "
+                              << "Asked to abort turn. This is not implemented";
     }
     
     virtual std::future<Turn> doMakeTurn(GameState state) override {
@@ -44,12 +49,14 @@ public:
             Turn turn = (turnIt == std::end(turns)) ? Turn() : (*turnIt);
             int duration = m_msDist(m_rng);
 
-            LOG(Logging::debug) << "Will select " << turn << " after " << duration << " seconds sleep";
+            LOG(Logging::debug) << "(" << m_seed << ") "
+                                << "Will select " << turn << " after " << duration << " seconds sleep";
             std::this_thread::sleep_for(std::chrono::seconds(duration));
             return turn;
         });
     }
 private:
+    const unsigned int m_seed;
     std::mt19937 m_rng;
     std::uniform_int_distribution<int> m_msDist;
     Logging::Logger m_log;
